bb6b4740364d98eb74e3c76264f1babb9576da02
leolanavo <leo0198@gmail.com>
2017-03-20 18:25:57 +0000

    Add the ST methods

--- EP1/ST.java
+++ EP1/ST.java
@@ -4,64 +4,102 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.Stopwatch;
 
 public class ST {
+    int length;
+    int[] freq;
+    String[] words;
     
     // Create an empty symbol table
     public ST() {
+        length = 256;
+        freq = new int[length];
+        words = new String[length];
     }
 
     // Returns the value associated with the given key if the key is in the symbol table
     // and -1 if the key is not in the symbol table
     // Throws java.lang.NullPointerException - if key is null
     public int get(String key) {
-        if (key == null) {
+        
+        if (key == null) 
             throw new java.lang.NullPointerException("ST.get(): key is null");
-        }
-        // escreva seu método get() aqui
+        
+        int i = findPos(key);
+
+        if (!(key.equals(words[i]))) return (freq[i]);
+        else return -1;
     }
 
     // Insert the key into the symbol table and increase its value
     // Throws java.lang.NullPointerException - if key is null
     public void put(String key) {
-        if (key == null) {
-            throw new java.lang.NullPointerException("ST.put(): key is null");
-        }
-        // escreva seu métod put() aqui
+
+        if (key == null) 
+            throw new java.lang.NullPointerException("ST.put(): key is null"); 
+        
+        int i = findPos(key);
+        
+        if (i == length) resize(2*length);
+        if (words[i] == null) words[i] = key;
+        freq[i]++;
     }
 
     
-    // Removes the key and associated value from the symbol table (if the key is in the symbol table).
+    // Removes the key and associated value from the symbol table (if the key
+    // is in the symbol table).
     // Throws java.lang.NullPointerException - if key is null
     public void delete(String key) {
-        if (key == null) {
+
+        if (key == null)
             throw new java.lang.NullPointerException("ST.delete(): key is null");
+
+        int i = findPos(key);
+
+        for (int i = findPos(key); i < length - 1 && words[i] != null; i++) {
+            words[i] = words[i + 1];
+            freq[i] = freq[i + 1];
         }
-        // escreva seu método delete() aqui
+        
+        i++;
+        words[i] = null;
+        freq[i] = 0;
+
+        if ((i - 1) == length/4) resize(length/4);
+
     }
 
     // Does this symbol table contain the given key?
     // Throws  java.lang.NullPointerException - if key is null
     public boolean contains(String key) {
-        if (key == null) {
+        if (key == null)
             throw new java.lang.NullPointerException("ST.contains(): key is null");
-        }
-        // escreva seu método contains() aqui
+        
+        int i = findPos(key);
+    
+        if (i == length) return false;
+        return true;
     }
 
     // Returns the number of key-value pairs in this symbol table
     public int size() {
-        // escreva seu métod size aqui
+        int i;
+        for (int i = 0; i < length && words[i] != null; i++);
+        return i;
     }
 
     // Is this symbol table empty?
     // Returns: true if this symbol table is empty and false otherwise
     public boolean isEmpty() {
-        // escreva seu métod isEmpty() aqui
+        if (words[0] != null) return false;
+        return true;
     }
 
     // Returns the largest (= maior frequência) key in the symbol table
     // Throws java.util.NoSuchElementException - if the symbol table is empty
     public String max() {
-        // escreva seu métod max() aqui
+        int i, max;
+        for (i = 0, max = freq[0]; i < length; i++)
+            if (max < freq[i]) max = freq[i];
+        return max;
     }
 
     // Returns a string representing the symbol table
@@ -74,9 +112,23 @@ public class ST {
 
     // Move the symbol table to one of size k
     private void resize(int k) {
-        // escreva seu método resize() aqui
+        String[] tmpstr = new String[k];
+        int[] tmpint = new int[k];
+        
+        for (int i = 0; i < length && words[i] != null; i++) {
+            tmpstr[i] = words[i];
+            tmpint[i] = freq[i];
+        }
+        
+        words = tmpstr;
+        freq = tmpint;
     }
 
+    private int findPos(String key) {
+        int i;
+        for (i = 0; i < length && !(key.equals(words[i])) && words[i] != null; i++);
+        return i;
+    }
 
     //-----------------------------------------------------------------
     // Exemplo de unit test para a ST
@@ -193,7 +245,7 @@ public class ST {
                 StdOut.println("'" + st2.max() + "'");
             }
             else if (s.equals(KEYS)) {
-                for (String key: st2.keys()) {
+                 for (String key: st2.keys()) {
                     StdOut.println(key);
                 }
             }
