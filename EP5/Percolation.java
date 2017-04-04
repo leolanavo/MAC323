982b79e32b89c08343337adbc20a1479425d76b0
leolanavo <leo0198@gmail.com>
2017-04-04 18:52:00 +0000

    Add the skeleton of all the methods needed

---
+++ EP5/Percolation.java
@@ -0,0 +1,22 @@
+public class Percolation {
+	// create n-by-n grid, with all sites initially blocked
+	public Percolation(int n) {}
+    
+    // open the site (row, col) if it is not open already
+	public void open(int row, int col) {}
+    
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {} 
+	
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {} 
+	
+    // number of open sites
+    public int numberOfOpenSites() {} 
+	
+    // does the system percolate?
+    public boolean percolates() {}
+	
+    // unit testing (required)
+    public static void main(String[] args) {} 
+}
