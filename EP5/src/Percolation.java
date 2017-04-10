c3c520dec5587cf617b44ff3836c0f72b198d5a5
leolanavo <leo0198@gmail.com>
2017-04-09 21:07:11 -0300

    Last commit

--- EP5/src/Percolation.java
+++ EP5/src/Percolation.java
@@ -8,20 +8,28 @@ public class Percolation {
     private int[][] grid;
     private int open_count;
     private final int dim;
-    private UnionFind UF;
-    private UnionFind UF_color;
+    private final int size;
+    private WeightedQuickUnionUF UF;
+    private WeightedQuickUnionUF UF_color;
 
 	// Create n-by-n grid, with all sites initially blocked
 	public Percolation(int n) {
         if (n <= 0) 
             throw new java.lang.IllegalArgumentException();
         dim = n;
+        size = n*n;
         open_count = 0;
         boolean color = false;
-        UF = new UnionFind(n, false);
-        UF_color = new UnionFind(n, true);
+        UF = new WeightedQuickUnionUF(size + 2);
+        UF_color = new WeightedQuickUnionUF(size + 1);
         grid = new int[n][n];
         
+        for (int i = 1; i <= dim; i++) {
+            UF.union(0, i);
+            UF.union(size + 1, size + 1 - i);
+            UF_color.union(0, i);
+        }
+
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 grid[i][j] = -1;
@@ -29,30 +37,32 @@ public class Percolation {
     
     // open the site (row, col) if it is not open already
 	public void open(int row, int col) {
-        if (row < 0 || row >= dim || col < 0 || col >= dim) return;
+        if (row < 0 || row >= dim || col < 0 || col >= dim)
+            throw new java.lang.IllegalArgumentException();
                                                              
+        int comp = dim*row + col + 1;
         if (grid[row][col] != -1) return;
         grid[row][col] = 0;
         open_count++;
         
         if (isOpen(row - 1, col)) {
-            UF.union(row - 1, col, row, col);
-            UF_color.union(row - 1, col, row, col);
+            UF.union(dim*(row - 1) + col + 1, comp);
+            UF_color.union(dim*(row - 1) + col + 1, comp);
         }
 
         if (isOpen(row, col + 1)) {
-            UF.union(row, col + 1, row, col);
-            UF_color.union(row, col + 1, row, col);
+            UF.union(dim*row + col + 2, comp);
+            UF_color.union(dim*row + col + 2, comp);
         }
 
         if (isOpen(row, col - 1)) {
-            UF.union(row, col - 1, row, col);
-            UF_color.union(row, col - 1, row, col);
+            UF.union(dim*row + col, comp);
+            UF_color.union(dim*row + col, comp);
         }
 
         if (isOpen(row + 1, col)) {
-            UF.union(row, col, row + 1, col);
-            UF_color.union(row, col, row + 1, col);
+            UF.union(comp, dim*(row + 1) + col + 1);
+            UF_color.union(comp, dim*(row + 1) + col + 1);
         }
 
         return; 
@@ -66,8 +76,9 @@ public class Percolation {
 	
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        if (row < 0 || row >= dim || col < 0 || col >= dim) return false;
-        return isOpen(row, col) && UF_color.connected(0, -1, row, col);
+        if (row < 0 || row >= dim || col < 0 || col >= dim)
+            throw new java.lang.IllegalArgumentException();
+        return isOpen(row, col) && UF_color.connected(0, dim*row + col + 1);
     } 
 	
     // number of open sites
@@ -78,11 +89,7 @@ public class Percolation {
     // does the system percolate?
     public boolean percolates() {
         if (numberOfOpenSites() < dim) return false;
-        for (int i = 0; i < dim; i++) {
-            if(UF.connected(0, -1, dim-1, i))
-                return true;
-        }
-        return false;
+        return UF.connected(0, size + 1);
     }
     
     // unit testing (required)
