77fe17c919020b569a36013427087c851d1654ef
leolanavo <leo0198@gmail.com>
2017-05-15 23:14:34 -0300

    First commit

---
+++ EP9/src/WordFinder.java
@@ -0,0 +1,26 @@
+import edu.princeton.cs.algs4.SeparateChainingHashST;
+
+
+public class WordFinder {
+   
+
+    public WordFinder(String[] str) {}
+
+    public String getMax() {
+        return null;
+    }
+
+    public String containedIn(int a, int b) {
+        return null;
+    }
+
+    public int[] appearIn(String str) {
+        int[] v = new int[1];
+
+        return v;
+    }
+
+    public static void main(String[] args) {
+    
+    }
+}
